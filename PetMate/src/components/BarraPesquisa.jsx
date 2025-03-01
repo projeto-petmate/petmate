4fa823b29bd6a0c4f891f06f4e0911f518b08b93
isadauzaker <jvictordauzaker@gmail.com>
2025-03-01 18:05:17 -0300

    atualização barra de pesquisa

--- PetMate/src/components/BarraPesquisa.jsx
+++ PetMate/src/components/BarraPesquisa.jsx
@@ -1,14 +1,25 @@
 import './BarraPesquisa.css';
-import { FaSearch } from "react-icons/fa";
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 function BarraPesquisa() {
+  const navigate = useNavigate();
+
+  const handleFocus = () => {
+    navigate('/adotar'); // Redireciona para a tela de adoção
+  };
+
   return (
     <div className='pesquisa-container'>
-      <input className='barraDePesquisa' type="text" placeholder="Digite aqui para encontrar seu novo amigo!" />
+      <input 
+        className='barraDePesquisa' 
+        type="text" 
+        placeholder="Digite aqui para encontrar seu novo amigo!" 
+        onFocus={handleFocus} // Chamando a função ao focar
+      />
       <FaSearch className='lupaIcon' />
-      {/* <img className='iconeLupa' src="/images/procurar.png" alt="" /> */}
     </div>
-  )
+  );
 }
 
-export default BarraPesquisa
+export default BarraPesquisa;
