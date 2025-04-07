6641281dd1872513b4393c86900efd232e7bee90
JOSÉ VINÍCIUS VICTOR DAUZAKER <jose_dauzaker@estudante.sesisenai.org.br>
2025-04-07 17:23:47 -0300

    tela de ADM 2.0

--- PetMate/src/pages/PerfilAdm.jsx
+++ PetMate/src/pages/PerfilAdm.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import './PerfilAdm.css';
+import { FaUsers } from "react-icons/fa";
+import { FaRegHandshake } from "react-icons/fa";
+import { MdOutlinePets } from "react-icons/md";
+import { BsChatRightHeart } from "react-icons/bs";
 
 function PerfilAdm() {
   return (
-    <div classname='container-painel-adm'>
-      <div className='NavbarAdm'></div>
-      <div className='Links-gerenciamento'></div>
+    <div>
+      <div className='NavbarAdm'>
+      <img className="nav-img" src="/images/petmate.svg" alt="logo_petmate" />
+      <h1 className='h1Adm'>Painel Administrativo</h1>
+      </div>
+      <div className='NavbarAdm-links'>
+      <div className="links-geneciamento">
+        <div className='icon-link'>
+        <FaUsers className='iconAdm'/>
+        <a className='linksAdm'>Usuários</a>
+        </div>
+
+        <div className="icon-link">
+        <FaRegHandshake className='iconAdm'/>
+        <a className='linksAdm'>ONGs</a>
+        </div>
+
+        <div className="icon-link">
+        <MdOutlinePets className='iconAdm'/>
+        <a className='linksAdm'>Pets</a>
+        </div>
+
+        <div className="icon-link">
+        <BsChatRightHeart className='iconAdm'/>
+        <a className='linksAdm'>Comentários</a>
+        </div>
+        
+      </div>
+      </div>
       <div className="containeradm"></div>
     </div>
   )
