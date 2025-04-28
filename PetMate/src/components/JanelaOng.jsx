9bd3eec53e1ac16f26251c85e187b566751d362b
josehvinii <jvictordauzaker@gmail.com>
2025-04-28 17:21:11 -0300

    Janela Ong atualizada

--- PetMate/src/components/JanelaOng.jsx
+++ PetMate/src/components/JanelaOng.jsx
@@ -2,6 +2,10 @@ import React, { useContext } from 'react'
 import './JanelaOng.css'
 import { OngContext } from '../contexts/OngContext'
 import { IoMdClose } from 'react-icons/io'
+import { IoLogoWhatsapp } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { PiInstagramLogoFill } from "react-icons/pi";
+
 
 export default function JanelaOng({ isOpen, setOpenModalOng }) {
     const { ong } = useContext(OngContext)
@@ -14,20 +18,25 @@ export default function JanelaOng({ isOpen, setOpenModalOng }) {
             <div className="container-janela-ong">
                 <div className="titulo-ong-modal">
                     <h2>{ong.nome_ong}</h2>
-                    <button onClick={() => setOpenModalOng(false)} className='botao-fechar-ong'>{<IoMdClose className='closeIcon' />}</button>
+                    <button onClick={() => setOpenModalOng(false)} className='botao-fechar-ong'>
+                        <IoMdClose className='closeIcon' />
+                    </button>
                 </div>
                 <div className="info-ong">
                     <div className="img-ong">
                         <img src={ong.foto_ong} alt="logo da ong" className='logo-ong' />
                     </div>
                     <div className="dados-ong">
-                        <div className="telefone-ong">
-                            <h3>Telefone de Contato:</h3>
-                            <p>{ong.telefone}</p>
-                        </div>
-                        <div className="telefone-denuncia">
-                            <h3>Telefone de Denúncias:</h3>
-                            <p>{ong.telefone_denuncia}</p>
+                        <div className="contato-redes">
+                            <div className="contato-ong">
+                                <h3>Contato:</h3>
+                                <p><IoLogoWhatsapp className="iconeWhats"/> <strong>WhatsApp:</strong> {ong.telefone}</p>
+                                <p><MdEmail className="iconeEmail" /> <strong>Email:</strong> {ong.email}</p>
+                            </div>
+                            <div className="redesSociais-ong">
+                                <h3>Redes Sociais:</h3>
+                                <p><PiInstagramLogoFill className="iconeInsta" /> <strong>Instagram:</strong> @{ong.instagram}</p>
+                            </div>
                         </div>
                         <div className="descricao-ong">
                             <h3>Descrição:</h3>
